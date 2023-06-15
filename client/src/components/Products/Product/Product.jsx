43adbcb162e38f3c2edfbba1b0c397fd202d1f66
aashishthakurcodes <j9883208@gmail.com>
2023-06-15 17:01:29 +0530

    Product section

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,6 +1,11 @@
 import "./Product.scss";
+
 const Product = () => {
-    return <div>Product</div>;
+    return (
+        <div className="products-container">
+            <h1>Hello Wolrd</h1>
+        </div>
+    );
 };
 
 export default Product;
